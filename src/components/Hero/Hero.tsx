22e46f8ccb2ab14f64de0ee43b01521a025bd7f9
kash2005 <kashmifernando3@gmail.com>
2024-08-18 19:10:55 +0530

    few changes

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import FoodPng from "../../assets/food.png";
+import Spoon from "../../assets/spoon.png";
+import Banana from "../../assets/banana2.png";
+import Leaf from "../../assets/leaf.png";
+import { IoCartOutline } from 'react-icons/io5';
+import { motion } from 'framer-motion';
+import { Variants } from 'framer-motion';
+
+const SlideDown = (delay: number): Variants => {
+    return {
+        initial: {
+            y: "100%",
+            opacity: 0,
+        },
+        animate: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.6,
+                delay: delay,
+            },
+        },
+    };
+};
+
+export const Hero = () => {
+  return (
+    <main>
+        <div className="container min-h-[600px] flex justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24 place-items-center justify-between">   
+                {/* text content here */}
+                <div className='space-y-3 mt-14 text-center md:text-left md:mt-0'>
+                    <motion.h1 
+                        variants={SlideDown(0.5)}
+                        initial="initial"
+                        whileInView="animate"
+                        className='relative text-5xl lg:text-7xl xl:text-8xl font-bold uppercase text-outline text-transparent'>Yummy
+                        <img src={Leaf} alt='' className='absolute w-[50px] top-0 right-0 md:right-[100px]'/>
+                    </motion.h1>
+                    <motion.h1 
+                        variants={SlideDown(1)}
+                        initial="initial"
+                        whileInView="animate"
+                        className='text-5xl lg:text-7xl xl:text-8xl font-bold uppercase'>
+                        BREAKFAST
+                    </motion.h1>
+                    <motion.p 
+                        variants={SlideDown(1.5)}
+                        initial="initial"
+                        whileInView="animate"
+                        className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nisi deserunt excepturi.</motion.p>
+                    <motion.button 
+                        variants={SlideDown(2)}
+                        initial="initial"
+                        whileInView="animate"
+                        className='btn-primary inline-block !mt-10'>
+                        <IoCartOutline className='inline mr-2' /> 
+                        Order Now
+                    </motion.button>
+                </div>
+                {/* images here */}
+                <div className='relative'>
+                    <motion.img 
+                        initial={{ opacity: 0, rotate: 20, x: 200, y:100}} 
+                        whileInView={{ opacity: 1, rotate: 0, x: 0, y: 0}}
+                        transition={{ duration: 0.8 }}
+                        src={FoodPng} alt='' className='w-[450px] img-shadow'/>
+                    <motion.img
+                        initial={{ opacity: 0, rotate: 120, x: 200, y:100}} 
+                        whileInView={{ opacity: 1, rotate: 75, x: 0, y: 0}}
+                        transition={{ duration: 0.8 }} 
+                        src={Spoon} alt="" className='w-[350px] absolute bottom-[-120px] -left-16 rotate-[75deg] img-shadow'/>
+                    <motion.img
+                        initial={{ opacity: 0, rotate: 20, x: 200, y:100}} 
+                        whileInView={{ opacity: 1, rotate: 0, x: 0, y: 0}}
+                        transition={{ duration: 0.8 }} 
+                        src={Banana} alt="" className='w-[400px] absolute top-[-30px] right-[-130px] md:right-[-160px] img-shadow' />
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -64,7 +64,49 @@ const SlideDown = (delay: number): Variants => {
 
 const Navbar: React.FC = () => {
   return (
-    
+    <nav>
+        <div className="container flex justify-between items-center font-league">
+            {/* logo section */}
+            <motion.img 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.5 }}
+                src={Logo} alt='' className='w-36'
+            />
+            {/* menu section */}
+            <div className='hidden md:block'>
+                <ul className='flex gap-6'>
+                    {
+                        NavMenu.map((menu) => (
+                            <motion.li
+                                variants={SlideDown(menu.delay)}
+                                initial="initial"
+                                animate="animate" 
+                                key={menu.id} className='nav-menu' 
+                                data-delay={menu.delay}>
+                                    <a 
+                                        href={menu.path} 
+                                        className='inline-block px-2 py-2 text-2xl'
+                                    >
+                                        {menu.title}
+                                    </a>
+                            </motion.li>
+                        )) 
+                    }
+                </ul>
+            </div>
+            {/* button section */}
+            <motion.div
+                variants={SlideDown(1)}
+                initial="initial"
+                animate="animate" 
+            >
+                <button className='h-[40px] w-[40px] grid place-items-center rounded-full text-white bg-dark'>
+                    <IoCartOutline />
+                </button>
+            </motion.div>
+        </div>
+    </nav>
   );
 }
 
